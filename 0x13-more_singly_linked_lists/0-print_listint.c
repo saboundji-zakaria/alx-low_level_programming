8914d0198fe258be8d3d0789a63659c16897eb96
saboundji-zakaria <zakaria.saboundji@hotmail.com>
2023-10-23 09:21:21 -0700

    fix file 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,17 +3,17 @@
 /**
  * print_listint - print the elements in the list
  * @h: the start of the list
- * Return: number of zsrw
+ * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-	unsigned int zsrw = 0;
+	unsigned int nodes = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		zsrw++;
+		nodes++;
 		h = h->next;
 	}
-	return (zsrw);
+	return (nodes);
 }
